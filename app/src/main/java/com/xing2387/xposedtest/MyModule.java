2736c9de90e122d9fc1397db39f29942e07b9b4a
xing2387 <xing2387@gmail.com>
2021-07-22 21:29:11 +0800

    增加android.provider.Settings$System#getString()的hook

--- app/src/main/java/com/xing2387/xposedtest/MyModule.java
+++ app/src/main/java/com/xing2387/xposedtest/MyModule.java
@@ -1,7 +1,15 @@
 package com.xing2387.xposedtest;
 
+import android.app.PendingIntent;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanSettings;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
 import android.net.wifi.WifiInfo;
+import android.os.Handler;
+import android.os.WorkSource;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -11,6 +19,7 @@ import java.lang.reflect.Method;
 import java.net.NetworkInterface;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -34,46 +43,59 @@ public class MyModule implements IXposedHookLoadPackage {
             }});
             put("getDeviceId", new ArrayList<>());
 
-//            //基站定位
-//            put("getCellLocation", new ArrayList<>());
-//            put("getAllCellInfo", new ArrayList<>());
-//
-//            //sim卡信息
-//            put("getSubscriberId", new ArrayList<String>() {{     //IMSI
-//                add("int");
-//            }});
-//            put("getSimSerialNumber", new ArrayList<String>() {{
-//                add("int");
-//            }});
-//            put("getNetworkOperatorForPhone", new ArrayList<String>() {{
-//                add("int");
-//            }});
-//            put("getSimCountryIsoForPhone", new ArrayList<String>() {{
-//                add("int");
-//            }});
-//            put("getSimOperatorNameForPhone", new ArrayList<String>() {{  //运营商
-//                add("int");
-//            }});
+            //基站定位
+            put("getCellLocation", new ArrayList<>());
+            put("getAllCellInfo", new ArrayList<>());
+
+            //sim卡信息
+            put("getSubscriberId", new ArrayList<String>() {{     //IMSI
+                add("int");
+            }});
+            put("getSimSerialNumber", new ArrayList<String>() {{
+                add("int");
+            }});
+            put("getNetworkOperatorForPhone", new ArrayList<String>() {{
+                add("int");
+            }});
+            put("getSimCountryIsoForPhone", new ArrayList<String>() {{
+                add("int");
+            }});
+            put("getSimOperatorNameForPhone", new ArrayList<String>() {{  //运营商
+                add("int");
+            }});
         }});
         put(WifiInfo.class.getName(), new HashMap<String, ArrayList<String>>() {{
             //MAC地址
             put("getMacAddress", new ArrayList<>());
             //获取IP地址
             put("getIpAddress", new ArrayList<>());
-//            //WiFi
-//            put("getSSID", new ArrayList<>());
-//            put("getBSSID", new ArrayList<>());
+            //WiFi
+            put("getSSID", new ArrayList<>());
+            put("getBSSID", new ArrayList<>());
         }});
         put(NetworkInterface.class.getName(), new HashMap<String, ArrayList<String>>() {{
             //MAC地址
             put("getHardwareAddress", new ArrayList<>());
         }});
-//        put("android.net.wifi.WifiManager", new HashMap<String, ArrayList<String>>() {{
-//            //WiFi
-//            put("getScanResults", new ArrayList<>());
-//        }});
+        put("android.net.wifi.WifiManager", new HashMap<String, ArrayList<String>>() {{
+            //WiFi
+            put("getScanResults", new ArrayList<>());
+            put("getConnectionInfo", new ArrayList<>());
+        }});
 
         put(Settings.Secure.class.getName(), new HashMap<String, ArrayList<String>>() {{
+            //android_id
+            put("getString", new ArrayList<String>() {{
+                //Settings.Secure.ANDROID_ID
+//            public static String getString(ContentResolver resolver, String name) {
+                add(ContentResolver.class.getName());
+                add(String.class.getName());
+            }});
+            //蓝牙信息
+//            put("getString", new ArrayList<String>() {{
+        }});
+
+        put(Settings.System.class.getName(), new HashMap<String, ArrayList<String>>() {{
             //android_id
             put("getString", new ArrayList<String>() {{
                 //Settings.Secure.ANDROID_ID
@@ -98,50 +120,60 @@ public class MyModule implements IXposedHookLoadPackage {
             }});
         }});
 
-//        put("android.hardware.SensorManager", new HashMap<String, ArrayList<Object>>() {{
-//            //传感器
-//            put("registerListener", new ArrayList<Object>() {{
-//                add(SensorEventListener.class);
-//                add(Sensor.class);
-//                add("int");
-//                add(Handler.class);
-//            }});
-//        }});
+        put("android.hardware.SensorManager", new HashMap<String, ArrayList<String>>() {{
+            //传感器
+            put("registerListener", new ArrayList<String>() {{
+                add(SensorEventListener.class.getName());
+                add(Sensor.class.getName());
+                add("int");
+                add(Handler.class.getName());
+            }});
+        }});
 
-//        //GPS
-//        put("android.location.LocationManager", new HashMap<String, ArrayList<Object>>() {{
-//        }});
+        //GPS
+        put("android.location.LocationManager", new HashMap<String, ArrayList<String>>() {{
+        }});
 
-//        put("android.accounts.AccountManager", new HashMap<String, ArrayList<String>>() {{
-//            //账户
-//            put("getAccounts", new ArrayList<>());
-//            put("getAccountsByType", new ArrayList<String>() {{
-//                add(String.class.getName());
-//            }});
-//        }});
-//        put("android.bluetooth.le.BluetoothLeScanner", new HashMap<String, ArrayList<String>>() {{
-//            //蓝牙信息
-//            put("startScan", new ArrayList<String>() {{
-//                add(List.class.getName());
-//                add(ScanSettings.class.getName());
-//                add(WorkSource.class.getName());
-//                add(ScanCallback.class.getName());
-//                add(PendingIntent.class.getName());
-//                add(List.class.getName());
-//            }});
-//        }});
+        put("android.accounts.AccountManager", new HashMap<String, ArrayList<String>>() {{
+            //账户
+            put("getAccounts", new ArrayList<>());
+            put("getAccountsByType", new ArrayList<String>() {{
+                add(String.class.getName());
+            }});
+        }});
+        put("android.bluetooth.le.BluetoothLeScanner", new HashMap<String, ArrayList<String>>() {{
+            //蓝牙信息
+            put("startScan", new ArrayList<String>() {{
+                add(List.class.getName());
+                add(ScanSettings.class.getName());
+                add(WorkSource.class.getName());
+                add(ScanCallback.class.getName());
+                add(PendingIntent.class.getName());
+                add(List.class.getName());
+            }});
+        }});
 
-//        put("android.app.ActivityManager", new HashMap<String, ArrayList<String>>() {{
-//            //安装列表
-//            put("getRunningAppProcesses", new ArrayList<>());
-//        }});
+        put("android.app.ActivityManager", new HashMap<String, ArrayList<String>>() {{
+            //安装列表
+            put("getRunningAppProcesses", new ArrayList<>());
+        }});
+
+        put("com.netease.gl.exoplayer.util.FreeFlowUtil", new HashMap<String, ArrayList<String>>() {{
+            //安装列表
+            put("isFreeFlow", new ArrayList<String>() {{
+                add(Context.class.getName());
+            }});
+        }});
     }};
-    //    com.tencent.smtt.sdk.TbsDownloader$1.handleMessage
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpp) {
         hookGL(lpp);
         hookGS(lpp);
+        hookWithPackageName(lpp, "com.example.hmcpdemo", "MyModule-hmcp");
+        hookWithPackageName(lpp, "com.netease.nis.smartercaptcha", "MyModule-yidun");
+        hookWithPackageName(lpp, "com.netease.freecardandroid", "MyModule-fc");
+
 //        hookGetImei(lpp);
     }
 
@@ -159,7 +191,7 @@ public class MyModule implements IXposedHookLoadPackage {
             return;
         }
 
-        Log.d(logTag, "555 handleLoadPackage: packageName = " + lpp.packageName + ", processName = " + lpp.processName);
+        Log.d(logTag, "1111 handleLoadPackage: packageName = " + lpp.packageName + ", processName = " + lpp.processName);
         Log.d(logTag, "package classloader: " + lpp.classLoader.toString());
 
 //        HashMap<String, HashMap<String, ArrayList<Object>>>
@@ -185,8 +217,8 @@ public class MyModule implements IXposedHookLoadPackage {
                 XposedBridge.hookMethod(method, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        if (("android.provider.Settings.Secure".equals(className) ||
-                                "android.provider.Settings$Secure".equals(className)) &&
+                        if (!TextUtils.isEmpty(className) &&
+                                className.startsWith("android.provider.Settings") &&
                                 "getString".equals(methodName)) {
                             Object value = param.args[param.args.length - 1];
                             if (value instanceof String) {
